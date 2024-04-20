ede0c10f8142dcf281d3f7187b066d5e59b8fce7
Afrizal Wahyu Adi Putra <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-04-20 17:11:36 +0700

    Create UMenu.pas

---
+++ Menu 3 level/UMenu.pas
@@ -0,0 +1,63 @@
+unit UMenu;
+
+interface
+
+procedure display_menu;
+procedure display_menu1;
+procedure display_menu2;
+
+implementation
+
+uses
+  SysUtils;
+
+procedure display_menu;
+begin
+  Writeln('=== Menu Program 1 ===');
+  Writeln('| 1. Program 1      |');
+  Writeln('| 2. Program 2      |');
+  Writeln('| 3. Program 3      |');
+  Writeln('| 4. Program 4      |');
+  Writeln('| 5. Program 5      |');
+  Writeln('| 6. Program 6      |');
+  Writeln('| 7. Program 7      |');
+  Writeln('| 8. Program 8      |');
+  Writeln('| 9. Program 9      |');
+  Writeln('| 10. Program 10    |');
+  Writeln('====================');
+end;
+
+procedure display_menu1;
+begin
+  // BISA DITAMBAHKAN ATAU DIGANTI DENGAN PROGRAM TULISAN
+  Writeln('=== Menu Program 2 ===');
+  Writeln('| 1. Program 11      |');
+  Writeln('| 2. Program 12      |');
+  Writeln('| 3. Program 13      |');
+  Writeln('| 4. Program 14      |');
+  Writeln('| 5. Program 15      |');
+  Writeln('| 6. Program 16      |');
+  Writeln('| 7. Program 17      |');
+  Writeln('| 8. Program 18      |');
+  Writeln('| 9. Program 19      |');
+  Writeln('| 10. Program 20     |');
+  Writeln('======================');
+end;
+
+procedure display_menu2;
+begin
+  Writeln('=== Menu Program 3 ===');
+  Writeln('| 1. Program 21      |');
+  Writeln('| 2. Program 22      |');
+  Writeln('| 3. Program 23      |');
+  Writeln('| 4. Program 24      |');
+  Writeln('| 5. Program 25      |');
+  Writeln('| 6. Program 26      |');
+  Writeln('| 7. Program 27      |');
+  Writeln('| 8. Program 28      |');
+  Writeln('| 9. Program 29      |');
+  Writeln('| 10. Program 30     |');
+  Writeln('======================');
+end;
+
+end.
