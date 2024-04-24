9e52125249c210df3505e3c34a4f241d7f542f84
Afrizal Wahyu Adi Putra <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-04-24 19:00:15 +0700

    Update main.pas

--- Menu 3 level/main.pas
+++ Menu 3 level/main.pas
@@ -3,51 +3,71 @@ program Main;
 uses UMenu, UProblem;
 
 var
-  level: Integer;
-  choice: Integer;
+  level: integer;
+  choice: integer;
 
 begin
   level := 1;
   while True do
   begin
-    if level = 1 then
-    begin
-      // Menampilkan menu 1-10
-      UMenu.display_menu;
-      Write('Pilih program dari menu dengan nomor diatas, ingin melanjutkan Program2 ketik (11) dan jika ingin Program3 ketik (99) : ');
-      Readln(choice);
-      if (choice < 1) or (choice > 10) then
-        level := 2
-      else
-        // Menjalankan program sesuai pilihan
-        UProblem.run_program(choice);
-    end
-    else if level = 2 then
-    begin
-      // Menampilkan pilihan untuk kembali ke UMenu.DisplayMenu() dengan memasukan angka 0
-      // Menampilkan Pilihan untuk kembali ke UMenu.DisplayMenu2() dengan memasukan angka 99
-      UMenu.display_menu1;
-      Write('Ketik 0 untuk Kembali ke Program1 atau 99 untuk ke menu selanjutnya: ');
-      Readln(choice);
-      if choice = 0 then
-        level := 1
-      else if choice = 99 then
-        level := 3
-      else
-        Writeln('Pilihan tidak valid.');
-    end
-    else if level = 3 then
-    begin
-      // Menampilkan menu 11-20
-      UMenu.display_menu2;
-      Write('Ketik 0 untuk Kembali ke Program1 atau 23 untuk ke program2: ');
-      Readln(choice);
-      if (choice >= 12) and (choice <= 22) then
-        // Menjalankan program sesuai pilihan
-        UProblem.run_program(choice)
-      else
-        level := 2;
+    case level of
+      1:
+      begin
+        UMenu.display_menu;
+        write('Pilih program dari menu: ');
+        readln(choice);
+        case choice of
+          1: level := 2;
+          2: level := 3;
+        else
+          writeln('Pilihan tidak valid.');
+        end;
+      end;
+
+      2:
+      begin
+        UMenu.display_menu1;
+        write('Pilih Program dari menu: ');
+        readln(choice);
+        case choice of
+          1: level := 3;
+          2: level := 4;
+          3: level := 1;
+        else
+          writeln('Pilihan tidak valid.');
+        end;
+      end;
+
+      3:
+      begin
+        UMenu.display_menu2;
+        write('Pilih program dari menu: ');
+        readln(choice);
+        if (choice >= 1) and (choice <= 10) then
+        begin
+          UProblem.run_program(choice);
+        end
+        else if choice = 11 then
+          level := 2
+        else if choice = 12 then
+          level := 1
+        else
+          writeln('Pilihan tidak valid.');
+      end;
+
+      4:
+      begin
+        UMenu.display_menu12;
+        write('Input 1 untuk kembali ke awal: ');
+        readln(choice);
+        if choice = 1 then
+          level := 1
+        else
+          writeln('Pilihan tidak valid.');
+      end;
+
+    else
+      writeln('Level tidak dikenali.');
     end;
   end;
 end.
-
