6ce4a04963113db7fe488af6ef2d6b33af1399bd
Afrizal Wahyu Adi Putra <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-04-24 19:01:08 +0700

    Update UMenu.pas

--- Menu 3 level/UMenu.pas
+++ Menu 3 level/UMenu.pas
@@ -5,59 +5,79 @@ interface
 procedure display_menu;
 procedure display_menu1;
 procedure display_menu2;
+procedure display_menu12;
 
 implementation
 
-uses
-  SysUtils;
+uses crt;
+
+procedure print_color(text: string; color: string);
+begin
+  case color of
+    'red': writeln(#27'[91m', text, #27'[00m');
+    'green': writeln(#27'[92m', text, #27'[00m');
+    'yellow': writeln(#27'[93m', text, #27'[00m');
+    'blue': writeln(#27'[94m', text, #27'[00m');
+  else
+    writeln(text);
+  end;
+end;
+
+procedure clear_screen;
+begin
+  clrscr;
+end;
 
 procedure display_menu;
 begin
-  Writeln('=== Menu Program 1 ===');
-  Writeln('| 1. Program 1      |');
-  Writeln('| 2. Program 2      |');
-  Writeln('| 3. Program 3      |');
-  Writeln('| 4. Program 4      |');
-  Writeln('| 5. Program 5      |');
-  Writeln('| 6. Program 6      |');
-  Writeln('| 7. Program 7      |');
-  Writeln('| 8. Program 8      |');
-  Writeln('| 9. Program 9      |');
-  Writeln('| 10. Program 10    |');
-  Writeln('====================');
+  clear_screen;
+  writeln('+===== Menu Utama =====+');
+  writeln('| 1. Menu Program 1    |');
+  writeln('| 2. Langsung ke UTS   |');
+  writeln('+======================+');
 end;
 
 procedure display_menu1;
 begin
-  // BISA DITAMBAHKAN ATAU DIGANTI DENGAN PROGRAM TULISAN
-  Writeln('=== Menu Program 2 ===');
-  Writeln('| 1. Program 11      |');
-  Writeln('| 2. Program 12      |');
-  Writeln('| 3. Program 13      |');
-  Writeln('| 4. Program 14      |');
-  Writeln('| 5. Program 15      |');
-  Writeln('| 6. Program 16      |');
-  Writeln('| 7. Program 17      |');
-  Writeln('| 8. Program 18      |');
-  Writeln('| 9. Program 19      |');
-  Writeln('| 10. Program 20     |');
-  Writeln('======================');
+  clear_screen;
+  writeln('+===== Menu Program  =====+');
+  writeln('| 1. Program UTS          |');
+  writeln('| 2. Menu Kosong :)       |');
+  writeln('+=========================+');
+  writeln('| 3. Kembali              |');
+  writeln('+=========================+');
+end;
+
+procedure display_menu12;
+begin
+  clear_screen;
+  writeln('+======= Menu kedua =======+');
+  writeln('|                          |');
+  writeln('|  UDAH DIBILANGIN KOSONG  |');
+  writeln('|                          |');
+  writeln('+==========================+');
+  writeln('|  Ketik 1 untuk ke awal   |');
+  writeln('+==========================+');
 end;
 
 procedure display_menu2;
 begin
-  Writeln('=== Menu Program 3 ===');
-  Writeln('| 1. Program 21      |');
-  Writeln('| 2. Program 22      |');
-  Writeln('| 3. Program 23      |');
-  Writeln('| 4. Program 24      |');
-  Writeln('| 5. Program 25      |');
-  Writeln('| 6. Program 26      |');
-  Writeln('| 7. Program 27      |');
-  Writeln('| 8. Program 28      |');
-  Writeln('| 9. Program 29      |');
-  Writeln('| 10. Program 30     |');
-  Writeln('======================');
+  clear_screen;
+  writeln('+==== Program UTS ====+');
+  writeln('| 1. Program 1        |');
+  writeln('| 2. Program 2        |');
+  writeln('| 3. Program 3        |');
+  writeln('| 4. Program 4        |');
+  writeln('| 5. Program 5        |');
+  writeln('| 6. Program 6        |');
+  writeln('| 7. Program 7        |');
+  writeln('| 8. Program 8        |');
+  writeln('| 9. Program 9        |');
+  writeln('| 10. Program 10      |');
+  writeln('+=====================+');
+  writeln('| 11. Menu sebelumnya |');
+  writeln('| 12. Kembali ke awal |');
+  writeln('+=====================+');
 end;
 
 end.
