7c3d2c41455a4a2f99efbd08d2b30dcd7e29690f
Afrizal Wahyu Adi Putra <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-04-25 17:14:27 +0700

    Update UTS-ANS7.py

--- Menu 3 level/Python/UTS-ANS7.py
+++ Menu 3 level/Python/UTS-ANS7.py
@@ -1,12 +1,25 @@
-import UMenu
-import UProses
+import UMenu # import di python bisa dibuat perintah untuk melakukan link pada sebuah file 
+import UProses # import di python bisa dibuat perintah untuk melakukan link pada sebuah file
 
-def main():
+def main(): # Untuk tampilan utama pada file UTS-ANS7.py
     level = 1 # Tampilan awal saat menjalankan program di "level 1"
     while True: # Jika dalam kondisi benar Maka bab utama ini akan terus mengulangi ke kondisi utama atau awal
         if level == 1: # Deklarasi dan perkenalan bahwa bab ini adalah "level 1"
             UMenu.display_menu() # NamaFile.BabPadaFile
-            choice = int(input("Pilih program dari menu: ")) # Ouput untuk ditampilkan dibawah tabel bab "level 1" 
+            choice = int(input(
+            "\033[094m┌──(" #[094m : warna biru
+            "\033[1m" # [1m : Bold
+            "\033[091mUTS㉿ANS7" # [091m : Merah 
+            "\033[0m" # [0m : Reset kembali ke seperti awal
+            "\033[094m)-[" # 
+            "\033[1m" 
+            "\033[37mPilih Menu" # [37m : Warna Putih
+            "\033[094m]\n" # \n : bisa di istilah kan sebuah (enter) ketika kita mengetik
+            "\033[094m└─"
+            "\033[1m"
+            "\033[091m> "
+            "\033[0m"
+            )) # Ouput untuk ditampilkan dibawah tabel bab "level 1" 
             if choice == 1: # Ketika kita memasukan angka 1
                 level = 2 # Pemindahan level 2
             elif choice == 2 : # Ketika kita memasukan angka 2
@@ -16,7 +29,20 @@ def main():
                 
         elif level == 2:
             UMenu.display_menu1()
-            choice = int(input("Pilih Program dari menu: "))
+            choice = int(input(
+            "\033[094m┌──("
+            "\033[1m"
+            "\033[091mUTS㉿ANS7"
+            "\033[0m"
+            "\033[094m)-["
+            "\033[1m"
+            "\033[37mPilih Menu"
+            "\033[094m]\n"
+            "\033[094m└─"
+            "\033[1m"
+            "\033[091m> "
+            "\033[0m"
+            ))
             if choice == 1:
                 level = 3
             elif choice == 2:
@@ -28,11 +54,16 @@ def main():
                 
         elif level == 3:
             UMenu.display_menu2()
-            choice = int(input("Pilih program dari menu: "))
-            if choice >= 1 and choice <= 12:
+            choice = int(input(
+            "\033[033m Pilih "
+            "\033[1m"
+            "\033[37m> "
+            "\033[0m"
+            ))
+            if choice >= 1 and choice <= 12: # Codingan ini diperintahkan untuk menginput data atau angka dari 1 - 12
                 UProses.run_program(choice)
-            elif choice == 13:
-                  level = 2
+            elif choice == 13: # elif disini berarti mempunyai kondisi berbeda dari "if" sebelumnya di angka 13
+                  level = 2 # ketika melakukan input di angka "13" maka kita akan dialihkan ke "bab 2" atau yang disebut "level 2"
             elif choice == 14:
                   level = 1
             else:
@@ -40,7 +71,12 @@ def main():
 
         elif level == 4:
             UMenu.display_menu12()
-            choice = int(input("Input 1 untuk kembali ke awal: "))
+            choice = int(input(
+            "\033[033m Pilih " # [033m : Warna Coklat
+            "\033[1m"
+            "\033[37m> "
+            "\033[0m"
+            ))
             if choice == 1:
                 level = 1
             else:
